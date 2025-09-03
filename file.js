cbb028064fc1d2d823b73392c6c0d9f295d67e43
Thiru <131335194+Thiru7869@users.noreply.github.com>
2025-09-03 11:09:12 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,5 +1,4 @@
 document.getElementById("cvDownloadBtn").addEventListener("click", function() {
-    const cvUrl = "https://drive.google.com/file/d/112bcjrMnYIDyP5OQxEbEg6AaEw5z93tS/view?usp=drive_link";
+    const cvUrl = "https://drive.google.com/uc?export=download&id=112bcjrMnYIDyP5OQxEbEg6AaEw5z93tS";
     window.location.href = cvUrl;
 });
-
