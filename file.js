553606f22e1ebb766c6fc65bed3eac71188c6b0c
Thiru <131335194+Thiru7869@users.noreply.github.com>
2025-12-22 16:29:57 +0530

    Update file.js

--- file.js
+++ file.js
@@ -1,4 +1,18 @@
-document.getElementById("cvDownloadBtn").addEventListener("click", function() {
-    const cvUrl = "https://drive.google.com/uc?export=download&id=112bcjrMnYIDyP5OQxEbEg6AaEw5z93tS";
-    window.open(cvUrl, "_blank");
+document.addEventListener("DOMContentLoaded", () => {
+    const backToTop = document.getElementById("backToTop");
+
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 300) {
+            backToTop.style.display = "block";
+        } else {
+            backToTop.style.display = "none";
+        }
+    });
+
+    backToTop.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
 });
